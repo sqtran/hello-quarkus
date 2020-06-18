371894da5fabd4c016b630c083093661530a9f75
Steve Tran <stran@redhat.com>
2020-06-18 12:23:57 -0400

    Add main method

---
+++ src/main/java/com/example/quarkus/hello/Main.java
@@ -0,0 +1,14 @@
+package com.example.quarkus.hello;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.annotations.QuarkusMain;
+
+@QuarkusMain
+public class Main {
+
+	public static void main(String[] args) {
+		 System.out.println("Running main method");
+	     Quarkus.run(args); 
+	}
+
+}
