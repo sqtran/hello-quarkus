7314bd770728838cbe0fc7b0f7770c1b1e9a81aa
Steve Tran <stran@redhat.com>
2021-05-23 16:30:33 -0400

    Add QuarkusTest annotation to UnitTest class

--- src/test/java/com/example/quarkus/hello/GreeterUnitTest.java
+++ src/test/java/com/example/quarkus/hello/GreeterUnitTest.java
@@ -3,7 +3,9 @@ package com.example.quarkus.hello;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import io.quarkus.test.junit.QuarkusTest;
 
+@QuarkusTest
 public class GreeterUnitTest {
 
     @Test
