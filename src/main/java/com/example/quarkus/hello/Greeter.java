9824503bce8d9d53b0392ce4e0ad817ac745b771
Steve Tran <stran@redhat.com>
2023-05-05 17:27:55 -0400

    Update Quarkus to latest 3.0.2.Final

--- src/main/java/com/example/quarkus/hello/Greeter.java
+++ src/main/java/com/example/quarkus/hello/Greeter.java
@@ -7,19 +7,18 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.ThreadLocalRandom;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-
 import org.jboss.logging.Logger;
 
 import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpServerRequest;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/")
 public class Greeter {
--- src/test/java/com/example/quarkus/hello/NativeGreeterIT.java
+++
@@ -1,9 +0,0 @@
-package com.example.quarkus.hello;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreeterIT extends GreeterTest {
-
-    // Execute the same tests but in native mode.
-}
