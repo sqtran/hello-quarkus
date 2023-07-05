117dee8bc8cb041c987a70941f39112dd5890d62
Steve Tran <stran@redhat.com>
2023-07-04 23:47:58 -0400

    Add logger to Main class

--- src/main/java/com/example/quarkus/hello/Main.java
+++ src/main/java/com/example/quarkus/hello/Main.java
@@ -1,14 +1,18 @@
 package com.example.quarkus.hello;
 
+import org.jboss.logging.Logger;
+
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
 @QuarkusMain
 public class Main {
 
+    private static final Logger logger = Logger.getLogger(Main.class);
+
 	public static void main(String[] args) {
-		 System.out.println("Running main method");
-	     Quarkus.run(args); 
+		 logger.info("Running main method");
+	     Quarkus.run(args);
 	}
 
 }
